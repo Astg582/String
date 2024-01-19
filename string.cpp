989b9ae382377a9f34a6215425b47dd8ba2b6b6f
Astg582 <109224508+Astg582@users.noreply.github.com>
2024-01-19 17:18:46 +0400

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "string.hpp"
+
+int main()
+{
+Str ob;
+ob.puch_back('A');
+ob.print();
+
+}
+
+
---
+++ string.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class Str{
+ptivate:
+	size_t m_size;
+	size_t m_cap;
+	char* m_ptr;
+public:
+	Str();
+	Str(size_t size, char val);
+	size_t size();
+	size_t cap();
+	void print();
+	void realloc();
+	void shrink_to_fit();
+	void push_back(char val);
+	bool empty();
+	void clear();
+	~Str();
+}
+
+
